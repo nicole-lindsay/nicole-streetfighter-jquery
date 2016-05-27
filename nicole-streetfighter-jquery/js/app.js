2e41aca761e560b262278f7cb7588761fb5f1e3e
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-05-26 23:31:42 -0400

    we have sound!

--- nicole-streetfighter-jquery/js/app.js
+++ nicole-streetfighter-jquery/js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 		$('.ryu-ready').hide();
 	})
 	.mousedown(function() {
-		//play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate(
@@ -24,3 +24,8 @@ $(document).ready(function() {
 		$('.ryu-ready').show();
 	});
 });
+function playHadouken () {
+	$('#hadouken-sound')[0].volume=0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
